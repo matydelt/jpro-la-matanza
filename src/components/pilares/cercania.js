2ea4e33cd43e72fdfcf216814114a761e260b58a
matydelt <matymanda@hotmail.com>
2022-01-02 17:40:39 -0300

    positivismo , futuro , cercania e historia componente agregado

--- src/components/pilares/cercania.js
+++ src/components/pilares/cercania.js
@@ -12,11 +12,11 @@ export default function Cercania({ dimension }) {
             <div>
                 <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
 
-                    <h5 style={{ width: "40%" }}>Todo esto empezó cuando en la crisis del 2001 todos los argentinos sentimos que los políticos habían dejado de representarnos. Fue ahí donde decidimos tomar una postura más activa, y convertirnos en protagonistas de la historia, cambiando las cosas nosotros mismos. Ese fue el impulso inicial que dio origen al PRO.</h5>
+                    <h5 style={dimension.width > 900 ? { width: "40%" } : { width: "90%" }}>Todo esto empezó cuando en la crisis del 2001 todos los argentinos sentimos que los políticos habían dejado de representarnos. Fue ahí donde decidimos tomar una postura más activa, y convertirnos en protagonistas de la historia, cambiando las cosas nosotros mismos. Ese fue el impulso inicial que dio origen al PRO.</h5>
                 </div>
                 <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
 
-                    <h5 style={{ width: "40%" }}>Estamos convencidos que debemos trabajar con valores, honestidad, trabajo y diálogo. Defendiendo la libertad y la democracia. Creemos que cambiar la realidad en la que vivimos lleva tiempo y esfuerzo. Y no nos resignamos a vivir así, porque creemos que vale la pena pelear por nuestro país, por nuestra provincia y por nuestro municipio.
+                    <h5 style={dimension.width > 900 ? { width: "40%" } : { width: "90%" }}>Estamos convencidos que debemos trabajar con valores, honestidad, trabajo y diálogo. Defendiendo la libertad y la democracia. Creemos que cambiar la realidad en la que vivimos lleva tiempo y esfuerzo. Y no nos resignamos a vivir así, porque creemos que vale la pena pelear por nuestro país, por nuestra provincia y por nuestro municipio.
                         Somos el futuro que hará historia y nos dará una oportunidad de vivir en nuestro país, queremos que vos también seas parte de esto!.</h5>
                 </div>
             </div>
